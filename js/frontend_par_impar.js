31916a03d49260dd06e2fcaaa593e6e5cb4c81ed
Jonathan MirCha <jonmircha@gmail.com>
2017-10-13 10:02:39 -0500

    Agregando repositorios

---
+++ js/frontend_par_impar.js
@@ -0,0 +1,10 @@
+function parImpar() {
+	let numero = prompt('Ingresa un número'),
+		modulo = numero % 2,
+		par = `El número: ${numero} es Par`,
+		impar = `El número: ${numero} es Impar`
+
+	return (modulo === 0) ? alert(par) : alert(impar)
+}
+
+parImpar()
